f1fe95a81e73f9af4e7375ef581fc585aa0ff9df
Juan Melguizo <juamelmos@alu.edu.gva.es>
2023-05-10 12:25:08 +0200

    Arreglamos un fallo

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "produccion" ? "/my-blog/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/my-blog/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
